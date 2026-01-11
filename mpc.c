da75599115d05a7047fb5ed485b7743b48724691
msbCyricTohoku <ben.sh@tohoku.ac.jp>
2026-01-12 02:23:06 +0800

    added main and time event placeholders

--- mpc.c
+++ mpc.c
@@ -11,5 +11,26 @@
 
 typedef struct {
   double time[N];
-  int status[N];} DATA;
+  int status[N];
+} DATA;
 
+
+double Z[N][P] //data matrix covars are const. at the moment
+
+int main(){
+
+  double T1[] = {};
+  double T2[] = {};
+  double T3[] = {};
+
+  double D1[] = {};
+  double D2[] = {};
+  double D3[] = {};
+
+
+  DATA S1, S2, S3; 
+
+
+  return 0;
+
+}
