db72e240777338d2ce273d81f08abc3b762cf7e2
msbCyricTohoku <ben.sh@tohoku.ac.jp>
2026-01-12 00:55:27 +0800

    defined key params for this based on lin's paper

--- mpc.c
+++ mpc.c
@@ -4,4 +4,9 @@
 
 //for now we hard code no. of subs
 #define N 36 //just like Lin's paper
+#define COVNO 2 //number of covariates
+#define TK 3 //number of time starta
+#define MAX_ITER 200 //max iteration for newtown raohson
+#define TOLERANCE 1e-16
+
 
