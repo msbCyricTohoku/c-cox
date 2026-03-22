dda05f941218d19199b12d8b3b227da9a7f50418
msbCyricTohoku <ben.sh@tohoku.ac.jp>
2026-03-23 01:54:24 +0800

    code cleanup

--- ccox.h
+++ ccox.h
@@ -1,3 +1,13 @@
+/*==============================================================================
+ c-cox: high performance cox regression
+ Version 2.0.0
+ ==============================================================================
+ Description: A high performance C implementation of Cox regression.
+
+ Authors:     msb
+ 
+ License:     Distributed under the GNU General Public License (GPL)
+==============================================================================*/
 #ifndef CCOX_H
 #define CCOX_H
 
@@ -19,14 +29,11 @@ typedef struct {
   double *robust_var;
 } DATA_RES;
 
-void U_I_Calc(DATA *data, int N, int COVNO, double beta[COVNO], double U[COVNO],
-              double I[COVNO][COVNO], double **Z, double *TiE1, int E1, int event_code);
+void U_I_Calc(DATA *data, int N, int COVNO, double beta[COVNO], double U[COVNO],double I[COVNO][COVNO], double **Z, double *TiE1, int E1, int event_code);
 
-void ccox(DATA *dat, DATA_RES *res, int N, int COVNO, double **Z, int MAX_ITER,
-          double TOLERANCE, int event_code, int robust);
+void ccox(DATA *dat, DATA_RES *res, int N, int COVNO, double **Z, int MAX_ITER, double TOLERANCE, int event_code, int robust);
 
-void compute_robust_variance(DATA *dat, DATA_RES *res, int N, int COVNO, double **Z, 
-                             double *TiE1, int E1, int event_code);
+void compute_robust_variance(DATA *dat, DATA_RES *res, int N, int COVNO, double **Z, double *TiE1, int E1, int event_code);
 
 void compute_cif(DATA *dat, int N, int event_code, int num_predict_times, double predict_times[]);
 
